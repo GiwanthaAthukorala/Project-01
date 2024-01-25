3357aeb86415047d3255499bd1e563b868f1ca77
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-01-26 01:09:26 +0530

    Add files via upload

---
+++ backend/src/data.js
@@ -0,0 +1,31 @@
+export const sample_foods = [
+  {
+    id: "1",
+    name: "pizza pepperoni",
+    cookTime: "10-20",
+    price: 10,
+    favorite: false,
+    origins: ["italy"],
+    stars: 4.5,
+    imageUrl: "pizza.jpg",
+    tags: ["FastFood", "Pizza", "Lunch"],
+  },
+
+  {
+    id: "2",
+    name: "Meatball",
+    cookTime: "20-30",
+    price: 52,
+    favorite: true,
+    origins: ["persia", "middle east", "china"],
+    stars: 4,
+    imageUrl: "O4wSmNb6w18.jpg",
+    tags: ["SlowFood", "Lunch"],
+  },
+];
+
+export const sample_tags = [
+  { name: "All", count: 6 },
+  { name: "FastFood", count: 4 },
+  { name: "Pizza", count: 2 },
+];
