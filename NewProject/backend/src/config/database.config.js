06cc1c97bdb652c7204cb6f3975c3a7ad64b1693
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-05 21:40:33 +0530

    Update database.config.js

--- NewProject/backend/src/config/database.config.js
+++ NewProject/backend/src/config/database.config.js
@@ -1,4 +1,11 @@
 import { connect, set } from "mongoose";
+import { UserModel } from "../Models/user.model.js";
+import { FoodModel } from "../Models/food.model.js";
+import { sample_users } from "../data.js";
+import { sample_foods } from "../data.js";
+import bcrypt from "bcryptjs";
+
+const PASSWORD_HASH_SALT_ROUNDS = 10;
 
 set("strictQuery", true);
 
@@ -8,8 +15,36 @@ export const dbconnect = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    await seedUsers();
+    await seedFoods();
     console.log("Connect Successfully...!!!");
   } catch (error) {
     console.log(error);
   }
 };
+
+async function seedUsers() {
+  const usersCount = await UserModel.countDocuments();
+  if (usersCount > 0) {
+    console.log("Users  Seed is Already Done...!");
+    return;
+  }
+  for (let user of sample_users) {
+    user.password = await bcrypt.hash(user.password, PASSWORD_HASH_SALT_ROUNDS);
+    await UserModel.create(user);
+  }
+  console.log("Users Seed is Done...!");
+}
+
+async function seedFoods() {
+  const foods = await FoodModel.countDocuments();
+  if (foods > 0) {
+    console.log("Foods Seed is Done...");
+    return;
+  }
+  for (const food of sample_foods) {
+    food.imageUrl = `/foods/${food.imageUrl}`;
+    await FoodModel.create(food);
+  }
+  console.log("Food Seed Is Done....!!");
+}
