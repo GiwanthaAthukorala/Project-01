f0b3d8cf1cab1e2fd92d49fef7b6e913724549b2
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-07 04:04:17 +0530

    Update food.router.js

--- NewProject/backend/src/routers/food.router.js
+++ NewProject/backend/src/routers/food.router.js
@@ -1,34 +1,70 @@
 import { Router } from "express";
-import { sample_foods, sample_tags } from "../data.js";
+import { FoodModel } from "../Models/food.model";
+import handler from "express-async-handler";
+import { sample_foods, sample_tags } from "../data";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send(sample_foods);
-});
+router.get(
+  "/",
+  handler(async (req, res) => {
+    const foods = await FoodModel.find({});
+    res.send(sample_foods);
+  })
+);
 
-router.get("/tags", (req, res) => {
-  res.send(sample_tags);
-});
+router.get(
+  "/tags",
+  handler(async (req, res) => {
+    const tags = await FoodModel.aggregate([
+      {
+        $unwind: "$tags",
+      },
+      {
+        $group: {
+          _id: "$tags",
+          count: { $sum: 1 },
+        },
+      },
+    ]).sort({ count: -1 });
 
-router.get("/search/:searchTerm", (req, res) => {
-  const { searchTerm } = req.params;
-  const foods = sample_foods.filter((item) =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  res.send(foods);
-});
+    const all = {
+      name: "All",
+      count: await FoodModel.countDocuments(),
+    };
+    tags.unshift(all);
 
-router.get("/tag/:tag", (req, res) => {
-  const { tag } = req.params;
-  const foods = sample_foods.filter((item) => item.tags?.includes(tag));
-  res.send(foods);
-});
+    res.send(sample_tags);
+  })
+);
 
-router.get("/:foodId", (req, res) => {
-  const { foodId } = req.params;
-  const food = sample_foods.find((item) => item.id === foodId);
-  res.send(food);
-});
+router.get(
+  "/search/:searchTerm",
+  handler(async (req, res) => {
+    const { searchTerm } = req.params;
+    const foods = sample_foods.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    res.send(foods);
+  })
+);
+
+router.get(
+  "/tag/:tag",
+  handler(async (req, res) => {
+    const { tag } = req.params;
+    const foods = sample_foods.filter((item) => item.tags?.includes(tag));
+    res.send(foods);
+  })
+);
+
+router.get(
+  "/:foodId",
+  handler(async (req, res) => {
+    const { foodId } = req.params;
+    const food = sample_foods.find((item) => item.id === foodId);
+    res.send(food);
+  })
+);
 
 export default router;
