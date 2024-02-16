bd046f595e3dc1c6ec4c2e80d9a8a3a0c3c28af1
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-17 02:12:00 +0530

    Update RegisterPage.js

--- NewProject/frontend/src/pages/Register/RegisterPage.js
+++ NewProject/frontend/src/pages/Register/RegisterPage.js
@@ -5,8 +5,22 @@ import Title from "../../components/Title/Title";
 import classes from "./registerPage.module.css";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { useEffect } from "react";
 
 export default function RegisterPage() {
+  const auth = useAuth();
+  const { user } = auth;
+  const navigate = useNavigate();
+  const [params] = useSearchParams();
+  const returnUrl = params.get("returnUrl");
+
+  useEffect(() => {
+    if (!user) return;
+    returnUrl ? navigate(returnUrl) : navigate("/");
+  }, [user]);
+
   const {
     handleSubmit,
     register,
@@ -14,7 +28,9 @@ export default function RegisterPage() {
     formState: { errors },
   } = useForm();
 
-  const submit = async (data) => {};
+  const submit = async (data) => {
+    await auth.register(data);
+  };
 
   return (
     <div className={classes.container}>
@@ -30,6 +46,54 @@ export default function RegisterPage() {
             })}
             error={errors.name}
           />
+          <Input
+            type="email"
+            label="Email"
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: /^[\w-.]+@([\w-]+\.)+[\w]{2,63}$/i,
+                message: "Email Is Not Valid",
+              },
+            })}
+            error={errors.email}
+          />
+          <Input
+            type="password"
+            label="Password"
+            {...register("password", {
+              required: true,
+              minLength: 8,
+            })}
+            error={errors.password}
+          />
+          <Input
+            type="password"
+            label="Confirm Password"
+            {...register("confirmPassword", {
+              required: true,
+              validate: (value) =>
+                value !== getValues("password") ? "Paswords Do No Match" : true,
+            })}
+            error={errors.confirmPassword}
+          />
+          <Input
+            type="text"
+            label="Address"
+            {...register("address", {
+              required: true,
+              minLength: 15,
+            })}
+            error={errors.address}
+          />
+          <Button type="Submit" text="Register" />
+
+          <div className={classes.login}>
+            Already a user ? &nbsp;
+            <Link to={`/login?${returnUrl ? "returnUrl=" + returnUrl : ""}`}>
+              Login Here
+            </Link>
+          </div>
         </form>
       </div>
     </div>
