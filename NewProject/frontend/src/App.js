656aec7014f24c63f3ddf2b190887e2b46d84c8b
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-22 18:18:17 +0530

    Update App.js

--- NewProject/frontend/src/App.js
+++ NewProject/frontend/src/App.js
@@ -1,9 +1,11 @@
 import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
+import Loading from "./components/Loading/Loading";
 
 function App() {
   return (
     <>
+      <Loading />
       <Header />
       <AppRoutes />
     </>
