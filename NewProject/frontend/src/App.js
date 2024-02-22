ebd1ccf8dcfaa1a42205c605993520439f85658e
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-02-22 23:41:15 +0530

    Update App.js

--- NewProject/frontend/src/App.js
+++ NewProject/frontend/src/App.js
@@ -1,8 +1,16 @@
 import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
 import Loading from "./components/Loading/Loading";
+import { setLoadingInterceptor } from "./interceptors/loadingInterceptor";
+import { useLoading } from "./hooks/useLoading";
+import { useEffect } from "react";
 
 function App() {
+  const { showLoading, hideLoading } = useLoading();
+
+  useEffect(() => {
+    setLoadingInterceptor({ showLoading, hideLoading });
+  }, []);
   return (
     <>
       <Loading />
