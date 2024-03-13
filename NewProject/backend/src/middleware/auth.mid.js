62fa0a4200b6398164148cf4c2eca8afd8e7b3b8
GiwanthaAthukorala <143745003+GiwanthaAthukorala@users.noreply.github.com>
2024-03-13 14:35:18 +0530

    Update auth.mid.js

--- NewProject/backend/src/middleware/auth.mid.js
+++ NewProject/backend/src/middleware/auth.mid.js
@@ -3,4 +3,14 @@ import { UNAUTHORIZED } from "../constants/httpStatus";
 
 export default (req, res, next) => {
   const token = req.headers.access_token;
+  if (!token) return res.status(UNAUTHORIZED).SEND();
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+  } catch (error) {
+    res.status(UNAUTHORIZED).send();
+  }
+
+  return next();
 };
